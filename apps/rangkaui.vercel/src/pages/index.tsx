83c799fe3ef5901e21dd94a7a9c23aa7354ccf0e
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-11 23:11:22 +0700

    feat: 🎸 move folder packages and apps

---
+++ apps/rangkaui.vercel/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from 'rangkaui-next';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Box p={12} style={{ background: 'black', color: 'white' }}>
+        awdaad
+      </Box>
+    </div>
+  );
+};
+
+export default HomePage;
