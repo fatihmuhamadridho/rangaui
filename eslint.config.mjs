14e0d7320891ef97807c27f5e87480adc27a8514
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-11 11:41:58 +0700

    feat: 🎸 update eslint tsconfig and npmignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginJs.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { ignores: ['**/*.{mjs,cjs,js,d.ts,d.mts}'] },
+  { languageOptions: { globals: globals.browser } },
 ];
