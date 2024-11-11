763a7443372d43bac0abf50ef4e1d40c735e9444
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-11 23:55:28 +0700

    feat: 🎸 implement mdx on nextjs

--- apps/rangkaui.vercel/next.config.ts
+++ apps/rangkaui.vercel/next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+import nextMDX from '@next/mdx';
 
+const withMDX = nextMDX({ extension: /\.mdx?$/ });
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withMDX({ ...nextConfig, pageExtensions: ['ts', 'tsx', 'md', 'mdx'] });
