0fc6caa83e2305a57aaeb41a3bc28935002605df
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-14 00:37:29 +0700

    feat: 🎸 update layout component in next js

--- apps/rangkaui.vercel/src/components/Layout/Layout.tsx
+++ apps/rangkaui.vercel/src/components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { MDXProvider } from '@mdx-js/react';
-import { Box, Flex } from 'rangkaui-next';
+import { Box } from 'rangkaui-next';
 
 interface MdxProviderProps {
   children: React.ReactNode;
@@ -13,24 +13,24 @@ const MdxProvider = ({ children }: MdxProviderProps) => {
     if (!isClient) setIsClient(true);
   }, [isClient]);
 
+  if (!isClient) return null;
+
   return (
     <Box className="min-h-screen h-full flex flex-col">
-      <Box className="h-14 border border-black">Navbar</Box>
-      <Flex className="flex-grow" style={{ height: 'calc(100vh - 56px)' }}>
-        <Box className="p-2 min-w-64 max-w-64 h-full border border-black overflow-y-auto">
-          Sidebar
-        </Box>
-        <Box className="p-2 flex-grow h-full overflow-y-auto">
-          <MDXProvider
-            components={{
-              h2: ({ children }) =>
-                isClient ? <h2 style={{ color: 'red' }}>{children}</h2> : <h2>{children}</h2>,
-            }}
-          >
-            {children}
-          </MDXProvider>
-        </Box>
-      </Flex>
+      <Box className="fixed p-2 w-full h-14 bg-white border-b border-black z-10">Navbar</Box>
+      <Box className="fixed mt-14 p-2 min-w-64 max-w-64 h-full bg-white border-r border-black overflow-y-auto">
+        Sidebar
+      </Box>
+      <Box className="pt-[calc(56px+8px)] pl-[calc(256px+8px)] p-2 flex-grow h-full overflow-y-auto">
+        <MDXProvider
+          components={{
+            h2: ({ children }) =>
+              isClient ? <h2 style={{ color: 'red' }}>{children}</h2> : <h2>{children}</h2>,
+          }}
+        >
+          {children}
+        </MDXProvider>
+      </Box>
     </Box>
   );
 };
