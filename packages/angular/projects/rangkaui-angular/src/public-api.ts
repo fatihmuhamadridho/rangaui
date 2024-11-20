f5b18b08302e0954b8f0ee7a55326a8e152fb4b2
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-21 00:26:43 +0700

    fix: 🐛 button export module

--- packages/angular/projects/rangkaui-angular/src/public-api.ts
+++ packages/angular/projects/rangkaui-angular/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/rangkaui-angular.service';
 export * from './lib/rangkaui-angular.component';
 export * from './lib/rangkaui-angular.module';
 export * from './lib/button/button.component';
+export * from './lib/button/button.module';
