09bd9681c5f2ba64ab4c093afa5a3a16f9614a43
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-11-25 01:06:29 +0700

    feat: 🎸 remove rangkaui angular from this project

--- packages/angular/projects/rangkaui-angular/src/public-api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of rangkaui-angular
- */
-
-export * from './lib/rangkaui-angular.service';
-export * from './lib/rangkaui-angular.component';
-export * from './lib/rangkaui-angular.module';
-export * from './lib/button/button.component';
-export * from './lib/button/button.module';
