c8b61798666191bd03121ce2a0adfe4abefa694e
fatihmuhamadridho <fatih.ridho@salt.co.id>
2024-12-12 23:20:38 +0700

    feat: 🎸 update navbar and component button

--- apps/rangkaui.vercel/src/components/Navbar/Navbar.tsx
+++ apps/rangkaui.vercel/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box } from 'rangkaui-next-dev';
+import { Box, Button, Stack } from 'rangkaui-next-dev';
 import { useTheme } from '@/contexts/theme';
 
 const Navbar = () => {
@@ -9,7 +9,29 @@ const Navbar = () => {
       className="fixed mt-14 p-2 min-w-64 max-w-64 h-full overflow-y-auto"
       style={{ color, backgroundColor: '#1f1f1f' }}
     >
-      Sidebar
+      <Stack gap={0}>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          Getting Started
+        </Button>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          About Rangkaui
+        </Button>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          API Overview
+        </Button>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          Contribute
+        </Button>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          Colors Generator
+        </Button>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          Help Center
+        </Button>
+        <Button className="min-h-[48px]" w={'100%%'} bg={'#228be633'} fz={14}>
+          Rangkaui
+        </Button>
+      </Stack>
     </Box>
   );
 };
